1a5eadaf236b8630e79197a4f4a7e11276008eb0
diffident016 <marvin.tagolimotjr13@gmail.com>
2022-12-29 23:50:41 +0800

    feat: added table widget + error alert dialog

---
+++ lib/widgets/appointment_data.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:online_clinic_appointment/models/appointment.dart';
+import 'package:online_clinic_appointment/utils.dart';
+import 'package:syncfusion_flutter_datagrid/datagrid.dart';
+
+class AppointmentDataSource extends DataGridSource {
+  /// Creates the employee data source class with required details.
+  AppointmentDataSource({required List<Appointment> appoinments}) {
+    _appointmentData = appoinments
+        .asMap()
+        .entries
+        .map<DataGridRow>((e) => DataGridRow(cells: [
+              DataGridCell<int>(columnName: 'no', value: e.key + 1),
+              DataGridCell<String>(
+                  columnName: 'time',
+                  value: Utils.displayTime(e.value.schedule.time)),
+              DataGridCell<String>(
+                  columnName: 'name', value: e.value.patient.name),
+              DataGridCell<String>(
+                  columnName: 'status',
+                  value: e.value.payment ? 'Done' : 'Pending'),
+            ]))
+        .toList();
+  }
+
+  List<DataGridRow> _appointmentData = [];
+
+  @override
+  List<DataGridRow> get rows => _appointmentData;
+
+  @override
+  DataGridRowAdapter buildRow(DataGridRow row) {
+    return DataGridRowAdapter(
+        cells: row.getCells().map<Widget>((e) {
+      return Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 2),
+        child: Text(
+          e.value.toString(),
+          style: const TextStyle(fontSize: 13),
+          overflow: TextOverflow.ellipsis,
+        ),
+      );
+    }).toList());
+  }
+}
---
+++ lib/widgets/table_widget.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:online_clinic_appointment/models/appointment.dart';
+import 'package:online_clinic_appointment/widgets/appointment_data.dart';
+import 'package:syncfusion_flutter_datagrid/datagrid.dart';
+
+class TableWidget extends StatefulWidget {
+  final AppointmentDataSource appointmentDataSource;
+  final Function(int index) openAppointment;
+  const TableWidget(
+      {Key? key,
+      required this.appointmentDataSource,
+      required this.openAppointment})
+      : super(key: key);
+
+  @override
+  TableWidgetState createState() => TableWidgetState();
+}
+
+class TableWidgetState extends State<TableWidget> {
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width - 40;
+
+    return SfDataGrid(
+      source: widget.appointmentDataSource,
+      columnWidthMode: ColumnWidthMode.fill,
+      headerRowHeight: 35,
+      rowHeight: 40,
+      onCellTap: (value) {
+        widget.openAppointment(value.rowColumnIndex.rowIndex - 1);
+      },
+      columns: <GridColumn>[
+        GridColumn(
+            columnName: 'no',
+            width: width * 0.12,
+            label: Container(
+                alignment: Alignment.center,
+                child: const Text(
+                  'No.',
+                ))),
+        GridColumn(
+            columnName: 'time',
+            width: width * 0.20,
+            label: Container(
+                alignment: Alignment.center,
+                child: const Text(
+                  'Time',
+                ))),
+        GridColumn(
+            columnName: 'name',
+            width: width * 0.46,
+            label: Container(
+                alignment: Alignment.center,
+                child: const Text(
+                  "Patient's Name",
+                ))),
+        GridColumn(
+            columnName: 'status',
+            width: width * 0.22,
+            label: Container(
+                alignment: Alignment.center,
+                child: const Text(
+                  'Status',
+                ))),
+      ],
+    );
+  }
+}
